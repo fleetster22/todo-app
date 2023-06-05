b43de5fc462add17796512b362c9f9c5ed89e276
Anaka Norfleet <fleetster22@gmail.com>
2023-06-05 14:53:20 -0400

    Feature 4 complete

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from projects.models import Project
 
-# Register your models here.
+
+@admin.register(Project)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")

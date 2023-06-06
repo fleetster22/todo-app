636e68ad64afbd89dfd660eb39081253d6985f7f
Anaka Norfleet <fleetster22@gmail.com>
2023-06-06 18:03:52 -0400

    Feature 15 complete

--- projects/models.py
+++ projects/models.py
@@ -1,10 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-# The Project model should have the following attributes:
-# name - string - max_length=200
-# description - string - no max_length
-# owner - foreign key to the User - Refers to the auth.User model, related # # # name "projects", on delete cascade, null is True
 
 
 class Project(models.Model):

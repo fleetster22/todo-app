a6210359d15ad11d4cb690ed741ae707e10fb525
Anaka Norfleet <fleetster22@gmail.com>
2023-06-06 12:05:00 -0400

    Feature 8 complete

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from projects.models import Project
 
 
+@login_required
 def list_projects(request):
-    list_project = get_list_or_404(Project)
+    user = request.user
+    list_project = Project.objects.filter(owner=user)
     context = {"list_projects": list_project}
 
     return render(request, "projects/project_list.html", context)
